ae49812d0656330b3cda546cf69bc6d73d0bbefe
Abhishek Sonwane <abhisheksonwane629@gmail.com>
2025-08-03 22:07:40 +0530

    first

---
+++ utils/data.js
@@ -0,0 +1,26 @@
+const users = [
+	{ name: "John Doe", referralCode: "JohnDoe2025", amountRaised: 2500 },
+	{ name: "Jane Smith", referralCode: "JaneSmith2025", amountRaised: 1800 },
+	{
+		name: "Michael Johnson",
+		referralCode: "MichaelJohnson2025",
+		amountRaised: 3200,
+	},
+	{ name: "Emily Davis", referralCode: "EmilyDavis2025", amountRaised: 900 },
+	{ name: "David Wilson", referralCode: "DavidWilson2025", amountRaised: 1500 },
+	{ name: "Sophia Brown", referralCode: "SophiaBrown2025", amountRaised: 2750 },
+	{ name: "Chris Miller", referralCode: "ChrisMiller2025", amountRaised: 4100 },
+	{
+		name: "Olivia Taylor",
+		referralCode: "OliviaTaylor2025",
+		amountRaised: 600,
+	},
+	{
+		name: "Ethan Anderson",
+		referralCode: "EthanAnderson2025",
+		amountRaised: 1300,
+	},
+	{ name: "Ava Thomas", referralCode: "AvaThomas2025", amountRaised: 1950 },
+];
+
+module.exports = users;
